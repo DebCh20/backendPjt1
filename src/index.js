f8e43f934f6cc78a6dad750d775148aaa53e0807
debCh20 <18040208@kiit.ac.in>
2024-01-20 11:36:40 +0530

    Database connection done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
- 
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_Name } from "./constants.js";
+import express from "express";
+const app= express();
+dotenv.config({path:'./env'});
+
+;(async ()=>{
+    try {        
+        await mongoose.connect(process.env.MONGODB_URL+'/'+DB_Name)
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+    app.listen(process.env.PORT, ()=>{
+        console.log(`app is listening on port ${process.env.PORT}`);
+    })
+})()
